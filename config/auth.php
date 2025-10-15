31e1d1904bf7479442db4ec1b289f2fcb8adefea
AlexisR <rcaalexis10@gmail.com>
2025-10-15 00:39:29 -0600

    implementar autenticasion con modelo personalizado

--- config/auth.php
+++ config/auth.php
@@ -62,7 +62,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\Usuario::class),
+            'model' => env('AUTH_MODEL', App\Models\Usuario::class), //se agrego el modelo
         ],
 
         // 'users' => [
