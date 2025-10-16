5ea05a6f6f337ff524df5e6292e817e132203bb3
AlexisR <rcaalexis10@gmail.com>
2025-10-16 16:31:55 -0600

    feat(routes): endpoints para usuarios

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+use App\Http\Controllers\Api\UsuarioController;
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::prefix('usuarios')->group(function () {
+    Route::get('/', [UsuarioController::class, 'listarUsuarios']);
+    Route::get('/{id}', [UsuarioController::class, 'verUsuario']);
+    Route::post('/', [UsuarioController::class, 'crearUsuario']);
+    Route::put('/{id}', [UsuarioController::class, 'actualizarUsuario']);
+    Route::delete('/{id}', [UsuarioController::class, 'eliminarUsuario']);
+    Route::patch('/{id}/reactivar', [UsuarioController::class, 'reactivarUsuario']);
+});
