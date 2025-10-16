5080a5aa753c743581e0957a5560b062c7e2af70
AlexisR <rcaalexis10@gmail.com>
2025-10-16 16:44:16 -0600

    feat(routes): implementar autenticacion con Sanctum y agregar rutas para login y logout

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,23 @@
 <?php
 
-// use Illuminate\Http\Request;
-// use Illuminate\Support\Facades\Route;
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 use App\Http\Controllers\Api\UsuarioController;
-use App\Http\Controllers\Api\AuthController;
+
+
 
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::prefix('usuarios')->group(function () {
-    Route::get('/', [UsuarioController::class, 'listarUsuarios']);
-    Route::get('/{id}', [UsuarioController::class, 'verUsuario']);
-    Route::post('/', [UsuarioController::class, 'crearUsuario']);
-    Route::put('/{id}', [UsuarioController::class, 'actualizarUsuario']);
-    Route::delete('/{id}', [UsuarioController::class, 'eliminarUsuario']);
-    Route::patch('/{id}/reactivar', [UsuarioController::class, 'reactivarUsuario']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::prefix('usuarios')->group(function () {
+        Route::get('/', [UsuarioController::class, 'listarUsuarios']);
+        Route::get('/{id}', [UsuarioController::class, 'verUsuario']);
+        Route::post('/', [UsuarioController::class, 'crearUsuario']);
+        Route::put('/{id}', [UsuarioController::class, 'actualizarUsuario']);
+        Route::delete('/{id}', [UsuarioController::class, 'eliminarUsuario']);
+        Route::patch('/{id}/reactivar', [UsuarioController::class, 'reactivarUsuario']);
+    });
+
 });
