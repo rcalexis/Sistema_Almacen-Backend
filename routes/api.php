fc9e86f6a060336227aa13e670a5acc5f64d54d7
AlexisR <rcaalexis10@gmail.com>
2025-10-17 01:23:31 -0600

    feat(routes): Agregar rutas para Productos

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\UsuarioController;
-
-
+use App\Http\Controllers\Api\ProductoController;
+use App\Http\Controllers\Api\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -18,6 +18,16 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::put('/{id}', [UsuarioController::class, 'actualizarUsuario']);
         Route::delete('/{id}', [UsuarioController::class, 'eliminarUsuario']);
         Route::patch('/{id}/reactivar', [UsuarioController::class, 'reactivarUsuario']);
+    });
+
+    Route::prefix('productos')->group(function () {
+        Route::get('/', [ProductoController::class, 'listarProductos']);
+        Route::get('/{id}', [ProductoController::class, 'verProducto']);
+        Route::post('/', [ProductoController::class, 'crearProducto']);
+        Route::post('/{id}/aumentar', [ProductoController::class, 'aumentarInventario']);
+        Route::post('/{id}/sacar', [ProductoController::class, 'sacarInventario']);
+        Route::patch('/{id}/baja', [ProductoController::class, 'darBajaProducto']);
+        Route::patch('/{id}/reactivar', [ProductoController::class, 'reactivarProducto']);
     });
 
 });
