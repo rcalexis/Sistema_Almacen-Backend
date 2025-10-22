ef4d571124c52004270b26719115e206bb863efa
AlexisR <rcaalexis10@gmail.com>
2025-10-21 20:21:16 -0600

    fix: correcion en funcion para el filtro del historico

--- SCRIPTS/movimientos_funciones.sql
+++ SCRIPTS/movimientos_funciones.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION fn_listar_movimientos(p_tipo_movimiento VARCHAR DEFAULT NULL, p_usuario_id BIGINT)
+CREATE OR REPLACE FUNCTION fn_listar_movimientos(p_usuario_id BIGINT, p_tipo_movimiento VARCHAR DEFAULT NULL)
 RETURNS TABLE(
     id_movimiento BIGINT,
     producto_id BIGINT,
@@ -10,26 +10,40 @@ RETURNS TABLE(
     fecha_movimiento TIMESTAMP
 ) AS $$
 DECLARE
-    es_admin BOOLEAN;
+    v_rol VARCHAR;
 BEGIN
-    SELECT EXISTS(
-        SELECT 1
-        FROM usuarios u
-        JOIN roles r ON u.id_rol = r.id
-        WHERE u.id_usuario = p_usuario_id AND r.nombre = 'Administrador'
-    ) INTO es_admin;
+   
+    SELECT r.nombre INTO v_rol
+    FROM usuarios u
+    JOIN roles r ON u.id_rol = r.id
+    WHERE u.id_usuario = p_usuario_id;
 
-    IF NOT es_admin THEN
-        RAISE EXCEPTION 'Solo los administradores pueden ver el historial';
+
+    IF v_rol = 'Administrador' THEN
+      
+        RETURN QUERY
+        SELECT m.id_movimiento, m.producto_id, p.nombre as nombre_producto, m.usuario_id, u.nombre as nombre_usuario,
+               m.tipo_movimiento, m.cantidad, m.fecha_movimiento
+        FROM movimientos m
+        JOIN productos p ON m.producto_id = p.id_producto
+        JOIN usuarios u ON m.usuario_id = u.id_usuario
+        WHERE (p_tipo_movimiento IS NULL OR m.tipo_movimiento = p_tipo_movimiento)
+        ORDER BY m.fecha_movimiento DESC;
+
+    ELSIF v_rol = 'Almacenista' THEN
+       
+        RETURN QUERY
+        SELECT m.id_movimiento, m.producto_id, p.nombre as nombre_producto, m.usuario_id, u.nombre as nombre_usuario,
+               m.tipo_movimiento, m.cantidad, m.fecha_movimiento
+        FROM movimientos m
+        JOIN productos p ON m.producto_id = p.id_producto
+        JOIN usuarios u ON m.usuario_id = u.id_usuario
+        WHERE m.tipo_movimiento = 'salida'
+        ORDER BY m.fecha_movimiento DESC;
+
+    ELSE
+      
+        RAISE EXCEPTION 'No tiene permisos para ver el historial de movimientos.';
     END IF;
-
-    RETURN QUERY
-    SELECT m.id_movimiento, m.producto_id, p.nombre as nombre_producto, m.usuario_id, u.nombre as nombre_usuario,
-           m.tipo_movimiento, m.cantidad, m.fecha_movimiento
-    FROM movimientos m
-    JOIN productos p ON m.producto_id = p.id_producto
-    JOIN usuarios u ON m.usuario_id = u.id_usuario
-    WHERE (p_tipo_movimiento IS NULL OR m.tipo_movimiento = p_tipo_movimiento)
-    ORDER BY m.fecha_movimiento DESC;
 END;
 $$ LANGUAGE plpgsql;
